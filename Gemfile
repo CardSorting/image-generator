01dc9471d396a0cdfa5b43cf12d4440d31bc98c7
PlayMoreTCG <playmoretcg@gmail.com>
2025-02-10 23:00:12 -0700

    Add importmap-rails gem and configure import maps for JavaScript assets

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ group :test do
 end
 
 gem "kaminari", "~> 1.2"
+
+gem "importmap-rails", "~> 2.1"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,11 @@
-import { application } from "./application"
+// Import and register all your controllers from the importmap under controllers/*
 
-import StyleCardController from "./style_card_controller"
-application.register("style-card", StyleCardController)
+import { application } from "controllers/application"
 
-// Import and register other controllers...
-import GenerationController from "./generation_controller"
-application.register("generation", GenerationController)
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application)
